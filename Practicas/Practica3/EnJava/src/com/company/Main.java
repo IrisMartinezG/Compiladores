4113d6362c9576774355156f38abd5f2a77a9b78
IrisYMartinezG <irisyonitzimartinezgonzalez@outlook.com>
2021-01-22 13:24:19 -0600

    Migrando Funciones Mover y Ecerradura

--- Practicas/Practica3/EnJava/src/com/company/Main.java
+++ Practicas/Practica3/EnJava/src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.SortedMap;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        GestionadorAutomata prueba=new GestionadorAutomata();
+        prueba.readFile("prueba.af");
+        //GestionadorCadenas p=new GestionadorCadenas();
+        Automata a= prueba.getAutomata();
+        //System.out.println(a.esAFN());
+        ArrayList<Integer> conjuntonuevo=new ArrayList<>();
+        conjuntonuevo.add(1);
+        conjuntonuevo.add(2);
+        conjuntonuevo.add(3);
+        conjuntonuevo.add(4);
+        if(a.esAFN()){
+            SubConjuntos b=new SubConjuntos();
+            //b.Ecerradura(conjuntonuevo, a);
+            b.Mover(conjuntonuevo,'b',a);
+        }
+        //p.setAlfabeto(a.getSimbolo());
+
     }
 }
